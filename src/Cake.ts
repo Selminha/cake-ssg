6ae5a3e72f617bd265737bb7ac7f0a34dbbe1c60
selma <selma.shimono@gmail.com>
2021-07-25 15:22:58 -0300

    render index

--- src/Cake.ts
+++ src/Cake.ts
@@ -19,13 +19,20 @@ export class Cake {
     this.options = { ...defaultOptions, ...userOptions};
   }
 
-  private getFileContent(filename: string, ext: string): any{
-    // add other types as necessary
-    switch (ext)
-    {
-      case '.json': 
-        return (JSON.parse(fs.readFileSync(filename, { encoding: 'utf-8' })));
-    } 
+  private getFileContent(filename: string, ext: string): any {
+    try {
+      // add other types as necessary
+      switch (ext)
+      {
+        case '.json': 
+          return (JSON.parse(fs.readFileSync(filename, { encoding: 'utf-8' })));
+      } 
+    } catch (error) {
+      if (error.code !== 'ENOENT') throw error;
+      console.log('file ' + filename + ' not found');
+      return {};
+    }
+    
   }
 
   private getPageTemplatePath(builder: HandlebarsTemplateBuilder, parsedPath: path.ParsedPath):string {
@@ -40,6 +47,17 @@ export class Cake {
     return this.PAGE;
   }
 
+  private writeHtml(html: string, outDir: string, filename: string) {
+    const htmlDir = this.options.outputFolder + this.BAR + outDir;
+    if(html.length) {
+      fs.promises.mkdir(htmlDir, { recursive: true })
+      .then(x => fs.writeFile(htmlDir + this.BAR + filename + '.html',html, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      }));
+    }
+  }
+
   private renderPages(builder: HandlebarsTemplateBuilder) {
     const contentFiles = glob.sync(`${this.options.contentFolder}/**/*.*`);
     for (const contentFile of contentFiles) { 
@@ -48,20 +66,27 @@ export class Cake {
 
       const fileContents = this.getFileContent(contentFile, parsedPath.ext);      
       const templatepath = this.getPageTemplatePath(builder, parsedPath);
-      
+
       const html = builder.render(templatepath, fileContents);
-      const htmlDir = this.options.outputFolder + this.BAR + parsedPath.dir;
-      if(html.length) {
-        fs.promises.mkdir(htmlDir, { recursive: true })
-        .then(x => fs.writeFile(htmlDir + this.BAR + parsedPath.name + '.html',html, (err) => {
-          if (err) throw err;
-          console.log('The file has been saved!');
-        }));
-      }
+      this.writeHtml(html, parsedPath.dir, parsedPath.name);
     }
   }
 
+  // render index without content, it will be overwritten if content exists
+  private renderIndex(builder: HandlebarsTemplateBuilder) {
+    const templatepath = 'index';
+    if(!builder.exists(templatepath)) {
+      console.log('Error: index template not found');
+      return;
+    }
+
+    const html = builder.render(templatepath, {});
+    this.writeHtml(html, '', 'index');
+  }
+
   bake(): void {
-    this.renderPages(new HandlebarsTemplateBuilder(this.options));
+    const builder = new HandlebarsTemplateBuilder(this.options);
+    this.renderIndex(builder);
+    this.renderPages(builder);
   }
 }
