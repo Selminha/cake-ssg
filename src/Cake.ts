782096403dbd28e485c8b097d2f027f4e98bb5d0
selma <selma.shimono@gmail.com>
2021-07-25 23:57:28 -0300

    sections

--- src/Cake.ts
+++ src/Cake.ts
@@ -1,14 +1,16 @@
 import * as path from 'path';
-import * as fs from "fs";
+import * as fs from 'fs';
 import { CakeOptions }  from './CakeOptions';
 import { HandlebarsTemplateBuilder } from './HandlebarsTemplateBuilder';
-import { glob } from "glob";
+import { glob } from 'glob';
+import { Section } from './Section';
 
 export class Cake {
 
   private options: CakeOptions;
   private readonly BAR = '/';
-  private readonly PAGE = 'page'
+  private readonly PAGE = 'page';
+  private readonly SECTION = 'index';
 
   constructor(private userOptions?: CakeOptions) {
     const defaultOptions = {
@@ -21,17 +23,22 @@ export class Cake {
 
   private getFileContent(filename: string, ext: string): any {
     try {
-      // add other types as necessary
-      switch (ext)
-      {
-        case '.json': 
-          return (JSON.parse(fs.readFileSync(filename, { encoding: 'utf-8' })));
-      } 
+      var content = fs.readFileSync(filename, { encoding: 'utf-8' });
     } catch (error) {
       if (error.code !== 'ENOENT') throw error;
       console.log('file ' + filename + ' not found');
       return {};
-    }
+    }    
+    // add other types as necessary
+    switch (ext)
+    {
+      case '.json': 
+        if(content.length === 0)
+          return {};
+
+        return (JSON.parse(content));
+    } 
+    return {};
   }
 
   private getPageTemplatePath(builder: HandlebarsTemplateBuilder, parsedPath: path.ParsedPath):string {
@@ -56,13 +63,42 @@ export class Cake {
     }
   }
 
-  private renderPages(builder: HandlebarsTemplateBuilder) {
+  private getSections(): Record<string, Section> {
+    const contentFolders = glob.sync(`${this.options.contentFolder}/**/**`);
+    let sections: Record<string, Section> = {};
+    for (const contentFolder of contentFolders) {
+      if(contentFolder ==  this.options.contentFolder) {
+        // ignore root content folder
+        continue;
+      }
+      const contentFolderName = contentFolder.substring(this.options.contentFolder.length + this.BAR.length, contentFolder.length);
+      const parsedPath = path.parse(contentFolderName);
+      if(!sections[parsedPath.dir]) {
+        sections[parsedPath.dir] = { sections: [], pages: [] };
+      }
+      if(fs.lstatSync(contentFolder).isDirectory()) {
+        sections[parsedPath.dir].sections.push(parsedPath.name);
+      }
+      else {
+        sections[parsedPath.dir].pages.push(parsedPath.name);
+      }
+    }
+
+    return sections;
+  }
+
+  private renderHtml(builder: HandlebarsTemplateBuilder) {
+    const sections = this.getSections();
     const contentFiles = glob.sync(`${this.options.contentFolder}/**/*.*`);
     for (const contentFile of contentFiles) { 
       const contentFileName = contentFile.substring(this.options.contentFolder.length + this.BAR.length, contentFile.length);
       const parsedPath = path.parse(contentFileName);
 
-      const fileContents = this.getFileContent(contentFile, parsedPath.ext);      
+      const fileContents = this.getFileContent(contentFile, parsedPath.ext);    
+      
+      if(parsedPath.name === this.SECTION) {
+        console.log(contentFile);
+      }
       const templatepath = this.getPageTemplatePath(builder, parsedPath);
 
       const html = builder.render(templatepath, fileContents);
@@ -70,21 +106,8 @@ export class Cake {
     }
   }
 
-  // render index without content, it will be overwritten if content exists
-  private renderIndex(builder: HandlebarsTemplateBuilder) {
-    const templatepath = 'index';
-    if(!builder.exists(templatepath)) {
-      console.log('Error: index template not found');
-      return;
-    }
-
-    const html = builder.render(templatepath, {});
-    this.writeHtml(html, '', 'index');
-  }
-
   bake(): void {
     const builder = new HandlebarsTemplateBuilder(this.options);
-    this.renderIndex(builder);
-    this.renderPages(builder);
+    this.renderHtml(builder);
   }
 }
---
+++ src/Section.ts
@@ -0,0 +1,4 @@
+export interface Section {
+  sections: string[];
+  pages: string[];
+}
